674c1b7952bc4e2a653a8528d8d00460eaeb3ae9
Jessica Wang <jessiwang2000@gmail.com>
2023-03-08 22:14:46 -0800

    started merging OpenStates + LegInfo code; started bill text scraper; refactoring DB connection script

--- daily_updates.py
+++ daily_updates.py
@@ -8,6 +8,7 @@ from dateutil import parser
 import datetime
 import urllib.request
 from random import randint
+import logging
 
 app = Flask(__name__)
 
@@ -34,6 +35,7 @@ def get_bill_data_openstates():
 				"author": author
 			}
 			results_array.append(bill_data)
+	return results_array
 
 # TODO(juliacordero): Use pagination data to make sure we're getting all data.
 @app.route('/committee-data-openstates')
@@ -54,6 +56,8 @@ def get_committee_data_openstates():
 			webpage_link: homepage_link
 		}
 		results_array.append(cmte_data)
+	return results_array
+
 
 # TODO(juliacordero): house_vote_result
 @app.route('/house-vote-result-data-openstates')
@@ -64,43 +68,57 @@ def get_house_vote_result_data_openstates():
 # "id": "ocd-jurisdiction/country:us/state:ca/government",
 # "name": "California"
 
-### leginfo scraping ###
+# leginfo scraping #
+
+
 def make_soup(page: str, tag_pattern: str):  # HELPER FUNCTION
-    url = urllib.request.urlopen(page).read()
-    soup = bs(url, "html.parser")
-    return soup.select(tag_pattern)
+	url = urllib.request.urlopen(page).read()
+	soup = bs(url, "html.parser")
+	return soup.select(tag_pattern)
 
 
 def get_query(bill_number: str, session_year: str) -> str:  # HELPER FUNCTION
-    bill_number = bill_number.replace("-", "")
-    return f"{session_year}0{bill_number}"
+	bill_number = bill_number.replace("-", "")
+	return f"{session_year}0{bill_number}"
 
 
 def text_to_date_string(s: str) -> datetime.datetime:
-    try:
-        dt = parser.parse(s)
-        return dt.strftime("%Y-%m-%d")
-    except ValueError:
-        pass
+	try:
+		dt = parser.parse(s)
+		return dt.strftime("%Y-%m-%d")
+	except ValueError:
+		pass
 
 
-def bill_number_history(bill_number: str, sql_bill_id: int, session_year="20232024"):
-    """
-    Iterates over the entire history of actions related to bill BILL_NUMBER from session SESSION_YEAR according to the
-    bill's history page and prints current status to terminal.
-    """
-    bill_query = get_query(bill_number, session_year)
-    page = "https://leginfo.legislature.ca.gov/faces/billHistoryClient.xhtml?bill_id=" + bill_query
-    history_table_tag_pattern = "table[id='billhistory'] > tbody > tr"
-    history_soup = make_soup(page, history_table_tag_pattern)
-    action_records = []
-    chronological_order = 1
-    for i in reversed(range(len(history_soup))):
-        logging.debug(f"table row {i} detected, chronological order {chronological_order}.")
-        chronological_order += 1
-        date = history_soup[i].select("td")[0].text
-        action = history_soup[i].select("td")[1].text
-        logging.info(f"date detected: {date}")
-        logging.info(f"action recorded: {action}")
-        action_records.append((randint(0, 500), sql_bill_id, text_to_date_string(date), action))
-    return action_records
+def bill_number_history(bill_number: str, session_year="20232024"):
+	"""
+	Iterates over the entire history of actions related to bill BILL_NUMBER from session SESSION_YEAR according to the
+	bill's history page and prints current status to terminal.
+	"""
+	bill_query = get_query(bill_number, session_year)
+	page = "https://leginfo.legislature.ca.gov/faces/billHistoryClient.xhtml?bill_id=" + bill_query
+	history_table_tag_pattern = "table[id='billhistory'] > tbody > tr"
+	history_soup = make_soup(page, history_table_tag_pattern)
+	action_records = []
+	chronological_order = 1
+	for i in reversed(range(len(history_soup))):
+		logging.debug(f"table row {i} detected, chronological order {chronological_order}.")
+		chronological_order += 1
+		date = history_soup[i].select("td")[0].text
+		action = history_soup[i].select("td")[1].text
+		logging.info(f"date detected: {date}")
+		logging.info(f"action recorded: {action}")
+		record = {
+			"date": text_to_date_string(date),
+			"action": action
+		}
+		action_records.append(record)
+	return action_records
+
+
+def bill_number_text(bill_number: str, session_year="20232024"):
+	bill_query = get_query(bill_number, session_year)
+	page = "https://leginfo.legislature.ca.gov/faces/billTextClient.xhtml?bill_id=" + bill_query
+	text_tag = "div[id='bill_all']"
+	text_soup = make_soup(page, text_tag)[0]
+	return text_soup #TODO: confirm the desired data type for bill text
