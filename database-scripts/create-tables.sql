0151d9a7c5b549b2678ef505885eb0c59aea36d4
Jessica Wang <jessiwang2000@gmail.com>
2023-08-10 23:14:46 -0700

    rename house* tables and attributes with chamber

--- database-scripts/create-tables.sql
+++ database-scripts/create-tables.sql
@@ -9,7 +9,7 @@ create table if not exists ca.bill (
 	bill_number text,
 	full_text text,
 	author text,
-	origin_house_id integer,
+	origin_chamber_id integer,
 	committee_id integer,
 	status text
 );
@@ -21,13 +21,14 @@ create table if not exists ca.bill_history (
 	entry_text text
 );
 
-create table if not exists ca.house_vote_result (
-	hosue_vote_result_id integer primary key,
+create table if not exists ca.chamber_vote_result (
+	chamber_vote_result_id integer primary key,
 	vote_date date,
 	bill_id integer,
-	house_id integer,
+	chamber_id integer,
 	votes_for integer,
-	votes_against integer
+	votes_against integer,
+	votes_other integer
 );
 
 create table if not exists ca.committee_vote_result (
@@ -41,17 +42,25 @@ create table if not exists ca.committee_vote_result (
 
 
 
--- HOUSE AND COMMITTEE DATA
+-- CHAMBER AND COMMITTEE DATA
 
-create table if not exists ca.house (
-	house_id integer primary key,
+create table if not exists ca.chamber (
+	chamber_id integer primary key,
 	name text
 );
-insert into ca.house (house_id, name) values (1, 'Assembly'), (2, 'Senate');
+insert into ca.chamber (chamber_id, name) values (1, 'Assembly'), (2, 'Senate');
+
+create table if not exists ca.chamber_schedule (
+    schedule_id integer primary key,
+    chamber_id integer,
+    event_date text,
+    description text,
+    source text
+);
 
 create table if not exists ca.committee (
 	committee_id integer primary key,
-	house_id integer,
+	chamber_id integer,
 	name text,
 	webpage_link text
 );
--- database-scripts/drop-tables.sql
+++ database-scripts/drop-tables.sql
@@ -1,6 +1,7 @@
 drop table if exists ca.bill;
 drop table if exists ca.bill_history;
-drop table if exists ca.house_vote_result;
+drop table if exists ca.chamber_vote_result;
 drop table if exists ca.committee_vote_result;
-drop table if exists ca.house;
+drop table if exists ca.chamber;
 drop table if exists ca.committee;
+drop table if exists ca.chamber_schedule;
