9eb4b1c2ce3f94dcfabff9ed9530e356dd288dfa
Jessica Wang <jessiwang2000@gmail.com>
2023-08-10 21:29:04 -0700

    set default parameter on config.py

--- database-population/config.py
+++ database-population/config.py
@@ -1,7 +1,7 @@
 from configparser import ConfigParser
 
 
-def config(filename='credentials.ini', section='postgresql'):
+def config(section, filename='credentials.ini'):
     # create a parser
     parser = ConfigParser()
     # read config file
