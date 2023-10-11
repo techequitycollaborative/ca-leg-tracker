7eeb21c37b084f9bc8b9354579bc34cc23d10bbd
Jessica Wang <jessiwang2000@gmail.com>
2023-10-11 11:19:33 -0700

    Add misc columns to create-tables.sql for chamber votes and schedules

--- database-scripts/create-tables.sql
+++ database-scripts/create-tables.sql
@@ -35,7 +35,8 @@ create table if not exists ca.chamber_vote_result (
 	bill_id integer,
 	chamber_id integer,
 	votes_for integer,
-	votes_against integer
+	votes_against integer,
+	votes_other integer
 );
 
 create table if not exists ca.committee_vote_result (
@@ -56,6 +57,14 @@ create table if not exists ca.chamber (
 	name text
 );
 insert into ca.chamber (chamber_id, name) values (1, 'Assembly'), (2, 'Senate');
+
+create table if not exists ca.chamber_schedule (
+    schedule_id integer primary key,
+    chamber_id integer,
+    event_date text,
+    description text,
+    source text
+);
 
 create table if not exists ca.committee (
 	committee_id integer primary key,
