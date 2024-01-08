a3a5b39807c3690f2b50820408baceb2aeb1361f
Matt Brooks <matt@techequitycollaborative.org>
2024-01-08 13:20:24 -0800

    updated db schema

--- database-scripts/create-tables.sql
+++ database-scripts/create-tables.sql
@@ -4,48 +4,50 @@ create schema if not exists ca;
 -- BILL DATA
 
 create table if not exists ca.bill (
-	bill_id integer primary key,
-	bill_name text,
-	bill_number text,
-	user_name text,
-	full_text text,
-	author text,
-	origin_chamber_id integer,
-	committee_id integer,
-	status text
+    bill_id integer primary key,
+    bill_name text,
+    bill_number text,
+    full_text text,
+    author text,
+    coauthors text,
+    origin_chamber_id integer,
+    committee_id integer,
+    status text,
+    leginfo_link text,
+    leg_session text
 );
 
 create table if not exists ca.bill_history (
-	bill_history_id integer primary key,
-	bill_id integer,
-	entry_date date,
-	entry_text text
+    bill_history_id integer primary key,
+    bill_id integer,
+    entry_date date,
+    entry_text text
 );
 
 create table if not exists ca.bill_schedule (
-	bill_schedule_id integer primary key,
-	bill_id integer,
-	event_date date,
-	event_text text
+    bill_schedule_id integer primary key,
+    bill_id integer,
+    event_date date,
+    event_text text
 );
 
 create table if not exists ca.chamber_vote_result (
-	chamber_vote_result_id integer primary key,
-	vote_date date,
-	bill_id integer,
-	chamber_id integer,
-	votes_for integer,
-	votes_against integer,
-	votes_other integer
+    chamber_vote_result_id integer primary key,
+    vote_date date,
+    bill_id integer,
+    chamber_id integer,
+    votes_for integer,
+    votes_against integer,
+    votes_other integer
 );
 
 create table if not exists ca.committee_vote_result (
-	committee_vote_result_id integer primary key,
-	vote_date date,
-	bill_id integer,
-	committee_id integer,
-	votes_for integer,
-	votes_against integer
+    committee_vote_result_id integer primary key,
+    vote_date date,
+    bill_id integer,
+    committee_id integer,
+    votes_for integer,
+    votes_against integer
 );
 
 
@@ -53,8 +55,8 @@ create table if not exists ca.committee_vote_result (
 -- CHAMBER AND COMMITTEE DATA
 
 create table if not exists ca.chamber (
-	chamber_id integer primary key,
-	name text
+    chamber_id integer primary key,
+    name text
 );
 insert into ca.chamber (chamber_id, name) values (1, 'Assembly'), (2, 'Senate');
 
@@ -67,18 +69,80 @@ create table if not exists ca.chamber_schedule (
 );
 
 create table if not exists ca.committee (
-	committee_id integer primary key,
-	chamber_id integer,
-	name text,
-	webpage_link text
+    committee_id integer primary key,
+    chamber_id integer,
+    name text,
+    webpage_link text
 );
 
+create table if not exists ca.committee_assignment (
+    committee_assignment_id integer primary key,
+    legislator_id integer,
+    committee_id integer,
+    assignment_type text
+);
+
+create table if not exists ca.legislator (
+    legislator_id integer primary key,
+    chamber_id integer,
+    name text,
+    district integer,
+    party text
+);
+
+
+
 -- USER CUSTOMIZED DATA
+
+create table if not exists ca.app_user (
+    user_id integer primary key,
+    user_name text
+);
+
+create table if not exists ca.bill_community_sponsor (
+    bill_community_sponsor_id integer primary key,
+    bill_details_id integer,
+    community_org_id integer
+);
+
+create table if not exists ca.bill_dashboard (
+	bill_dashboard_id integer primary key,
+	dashboard_id integer,
+	bill_id integer
+);
+
+create table if not exists ca.bill_details (
+    bill_details_id integer primary key,
+    bill_dashboard_id integer,
+    alternate_name text,
+    policy_notes text,
+    org_position_id integer,
+    political_intel text,
+    assigned_user_id integer
+);
 
 create table if not exists ca.bill_issue (
     bill_issue_id integer primary key,
     issue_id integer,
-    bill_id integer
+    bill_details_id integer
+);
+
+create table if not exists ca.community_org (
+    community_org_id integer primary key,
+    community_org_name text
+)
+
+create table if not exists ca.dashboard (
+    dashboard_id integer primary key,
+    dashboard_name text
+);
+
+create table if not exists ca.discussion_comment (
+    discussion_comment_id integer primary key,
+    bill_dashboard_id integer,
+    user_id integer,
+    comment_datetime timestamp,
+    comment_text text
 );
 
 create table if not exists ca.issue (
@@ -86,11 +150,9 @@ create table if not exists ca.issue (
     issue_name text
 );
 
-create table if not exists ca.legislator_expected_vote (
-    legislator_expected_vote_id integer primary key,
-    legislator_id integer,
-    bill_id integer,
-    expected_vote text
+create table if not exists ca.org_position (
+    org_position_id integer primary key,
+    org_position_name text
 );
 
 create table if not exists ca.user_action (
