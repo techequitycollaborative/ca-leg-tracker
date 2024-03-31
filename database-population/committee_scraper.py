e16463d02c8e6261d7ff54fea12587cf4415318d
Jessica Wang <jessiwang2000@gmail.com>
2024-03-30 23:14:09 -0700

    Implement Senate cmte webpage URL scraper; confirm membership should not be scraped

--- database-population/committee_scraper.py
+++ database-population/committee_scraper.py
@@ -14,9 +14,23 @@ def make_soup(chamber):
         pat = "div[id='committees_wrapper']"
         chamber_url = assembly_cmte_home
     elif chamber == "senate":
-        pat = "div[id='block-view-committees-standing'] > div[class='content'] > div > div[class='view-content']"
+        pat = "ul[class='page-committees__committee-menu']"
         chamber_url = senate_cmte_home
     return scraper_utils.make_static_soup(chamber_url, pat)
+
+
+def extract_member_role(t):
+    s = t.text
+    member_role = s.split("(")
+    cmte_member_data = {
+        "link": t['href'],
+        "name": member_role[0].strip()
+    }
+    if len(member_role) == 1:
+        cmte_member_data["assignment_type"] = "Member"
+    elif len(member_role) == 2:
+        cmte_member_data["assignment_type"] = member_role[1].replace(")", "").strip()
+    return cmte_member_data
 
 
 def get_assembly_cmte_urls():
@@ -40,25 +54,44 @@ def get_assembly_cmte_members(cmte_url):
     source = scraper_utils.make_static_soup(url, pat)[0].find_all("tr")[1:]
     results = list()
     for i in source:
-        member_role = i.select_one("td > a").text.split("(")
-        cmte_member_data = None
-        if len(member_role) == 1:
-            cmte_member_data = {
-                "name": member_role[0].strip(),
-                "assignment_type": "Member"
-            }
-        elif len(member_role) == 2:
-            cmte_member_data = {
-                "name": member_role[0].strip(),
-                "assignment_type": member_role[1].replace(")", "").strip()
-            }
+        member_role = i.select_one("td > a").text
+        cmte_member_data = extract_member_role(member_role)
         results.append(cmte_member_data)
     return results
 
 
+def get_senate_cmte_urls():
+    source = make_soup("senate")[0].find_all("span", class_="field-content")
+    results = list()
+    for i in source:
+        cmte_name = i.text.strip()
+        cmte_url = i.find("a")["href"]
+        cmte_data = {
+            "name": cmte_name,
+            "webpage_link": cmte_url,
+            "chamber_id": 2
+        }
+        results.append(cmte_data)
+    return results
+
+
+def get_senate_cmte_members(cmte_url):
+    field_pat = "div[class='field-item even']"
+    source = scraper_utils.make_static_soup(cmte_url, field_pat)[0]
+    results = list()
+    header_tags = source.find_all("h2")
+    for h in header_tags:
+        if h.string == "Members:":
+            print(h.next_element.next_element.next_element)
+            # the line of code above provides evidence that Senate cmte members are not formatted predictably
+            # therefore, it's better to manually insert them into DB
+    return results
+
+
 def main():
-    for _ in get_assembly_cmte_urls():
-        print(_)
+    for u in get_senate_cmte_urls():
+        print(u)
+        print(get_senate_cmte_members(u['webpage_link']))
 
 
 if __name__ == "__main__":
