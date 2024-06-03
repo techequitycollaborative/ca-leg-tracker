77a0f181fb54ad4f1744de1de13cc10c84eab59a
Jessica Wang <jessiwang2000@gmail.com>
2024-06-03 10:50:52 -0700

    removed redundant grant statements

--- database-scripts/create-snapshot-tables.sql
+++ database-scripts/create-snapshot-tables.sql
@@ -51,13 +51,3 @@ create table [OPENSTATES_SCHEMA].bill_vote (
     other_count text
 );
 
-grant usage on schema [OPENSTATES_SCHEMA] to [BACKEND_USER];
-
-grant select on all tables in schema [OPENSTATES_SCHEMA] to [BACKEND_USER];
-
-grant update, insert, delete on
-    [OPENSTATES_SCHEMA].bill,
-    [OPENSTATES_SCHEMA].bill_sponsor,
-    [OPENSTATES_SCHEMA].bill_action,
-    [OPENSTATES_SCHEMA].bill_vote
-to [BACKEND_USER];
