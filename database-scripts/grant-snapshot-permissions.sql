8de8a27a217cc8ea3a5555ce7d3e07d50918f34c
Jessica Wang <jessiwang2000@gmail.com>
2025-02-13 01:26:52 -0800

    update sql grant file with new snapshot tables

--- database-scripts/grant-snapshot-permissions.sql
+++ database-scripts/grant-snapshot-permissions.sql
@@ -6,5 +6,7 @@ grant update, insert, delete on
     [OPENSTATES_SCHEMA].bill,
     [OPENSTATES_SCHEMA].bill_sponsor,
     [OPENSTATES_SCHEMA].bill_action,
-    [OPENSTATES_SCHEMA].bill_vote
+    [OPENSTATES_SCHEMA].bill_vote,
+    [OPENSTATES_SCHEMA].people,
+    [OPENSTATES_SCHEMA].people_roles
 to [BACKEND_USER];
